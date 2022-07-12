efccd35ec1259205d1e6e140646238691294c7ac
Laurent Dezitter <laurent.dezitter@gmail.com>
2022-07-12 17:44:22 -0400

    chore: setup tailwind

--- src/App.svelte
+++ src/App.svelte
@@ -4,10 +4,3 @@
 <main>
   <p>Work in progress</p>
 </main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-</style>
