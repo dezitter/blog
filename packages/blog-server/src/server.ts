fac42431fffe793732d864150fe1bd9478bb1599
Laurent Dezitter <laurent.dezitter@gmail.com>
2022-07-20 07:12:19 -0400

    feat(blog-server): add admin router

--- packages/blog-server/src/server.ts
+++ packages/blog-server/src/server.ts
@@ -1,8 +1,10 @@
 import Fastify from 'fastify';
+import { adminRouter } from './router';
 import { homeRouter } from './router';
 
 const server = Fastify({});
 
+server.register(adminRouter);
 server.register(homeRouter);
 
 export { server };
